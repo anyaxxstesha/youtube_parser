171075063a50c0082bdc2dc9c1e555acac65d298
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-05-02 16:48:27 +0300

    add create_database function realization

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from typing import Any
 from googleapiclient.discovery import build
+import psycopg2
 
 
 def get_youtube_data(api_key: str, channel_ids: list[str]) -> list[dict[str, Any]]:
@@ -23,3 +24,47 @@ def get_youtube_data(api_key: str, channel_ids: list[str]) -> list[dict[str, Any
             'videos': videos_data
         })
         return data
+
+
+def create_database(database_name: str, params: dict) -> None:
+    """Создание базы данных и таблиц для сохранения данных о каналах и видео"""
+
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f'DROP DATABASE {database_name}')
+    except psycopg2.errors.InvalidCatalogName:
+        pass
+
+    cur.execute(f'CREATE DATABASE {database_name}')
+    cur.close()
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE channels (
+                channel_id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                views INTEGER,
+                subscribers INTEGER,
+                videos INTEGER,
+                channel_url TEXT
+                )
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE videos (
+                video_id SERIAL PRIMARY KEY,
+                channel_id INT REFERENCES channels(channel_id),
+                title VARCHAR NOT NULL,
+                publish_date DATE,
+                video_url TEXT
+                )
+        """)
+
+    conn.commit()
+    conn.close()
